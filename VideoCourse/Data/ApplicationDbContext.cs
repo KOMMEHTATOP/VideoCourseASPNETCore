4257f53c8a7a6df521121362bd8e0066ce7f8968
Admin <123456+KOMMEHTATOP@users.noreply.github.com>
2025-03-17 15:38:44 +0500

    Add project files.

---
+++ VideoCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoCourse.Models;
+
+namespace VideoCourse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category{ get; set; }
+        public DbSet<ApplicationType> ApplicationType{ get; set; }
+    }
+}
