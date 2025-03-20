16eff67046fefc6587457b0c143090455691a84b
Admin <123456+KOMMEHTATOP@users.noreply.github.com>
2025-03-20 15:49:02 +0500

    Сделал вторую домашку.

--- VideoCourse/Controllers/ApplicationTypeController.cs
+++ VideoCourse/Controllers/ApplicationTypeController.cs
@@ -22,7 +22,7 @@ namespace VideoCourse.Controllers
         // GET - CREATE
         public IActionResult Create()
         {
-            
+
             return View();
         }
 
@@ -31,7 +31,68 @@ namespace VideoCourse.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
         {
-            _db.ApplicationType.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id==null||id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+         
+        // POST - Delete
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
